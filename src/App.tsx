447b07c9272ca3b69e77fe85ec185bf94c960742
Samir Idris <samzidris11@gmail.com>
2023-05-27 18:40:24 +0100

    complete typescript with basic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import { Greet } from './components/Greet'
+import { Person } from './components/Person'
+import { PersonList } from './components/PersonList'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const personName = {
+    first: "Bruce",
+    last: "Wayne"
+  }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Clark',
+      last: 'Kent'
+    },
+    {
+      first: 'Princess',
+      last: 'Diana'
+    }
+  ]
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Person name={personName} />
+      <Greet name="Samir" messageCount={20} isLoggedIn={false} />
+      <PersonList names={nameList} />
     </>
   )
 }
---
+++ src/components/Greet.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+type GreetProps = {
+  name: string
+  messageCount: number
+  isLoggedIn: boolean
+}
+
+export const Greet = (props: GreetProps) => {
+  return (
+    <div>
+      <h2>
+        {props.isLoggedIn
+          ? `Welcome ${props.name}! You have ${props.messageCount} unread messages`
+          : `Welcome Guest`}
+      </h2>
+    </div>
+  )
+}
