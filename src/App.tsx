3f41558911d1295e8235cdb3891900a7a0c93aad
Samir Idris <samzidris11@gmail.com>
2023-05-28 15:04:25 +0100

    complete style props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import './App.css'
-import { Button } from './components/Button'
-import { Input } from './components/Input'
-
+import { Container } from './components/Container'
 
 function App() {
   return (
     <>
-      <Button
-        handleClick={(event, id) => {
-          console.log("Button clicked", event, id)
-        }}
-      />
-      <Input value="" handleChange={event => console.log(event)} />
+      <Container styles={{ border: '1px solid black', padding: '1 rem' }} />
     </>
   )
 }
