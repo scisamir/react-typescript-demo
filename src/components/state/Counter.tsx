e0368b459732b36d6746488c3f0a12fde4149079
Samir Idris <samzidris11@gmail.com>
2023-05-30 18:06:06 +0100

    complete usereducer strict action types

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -5,10 +5,16 @@ type CounterState = {
   count: number
 }
 
-type CounterAction = {
-  type: string
+type UpdateAction = {
+  type: 'increment' | 'decrement'
   payload: number
 }
+
+type ResetAction = {
+  type: 'reset'
+}
+
+type CounterAction = UpdateAction | ResetAction
 
 const initialState = { count: 0 };
 
@@ -18,6 +24,8 @@ const reducer = (state: CounterState, action: CounterAction) => {
       return { count: state.count + action.payload };
     case 'decrement':
       return { count: state.count - action.payload };
+    case 'reset':
+      return initialState;
     default:
       return state;
   }
@@ -32,6 +40,7 @@ export const Counter = () => {
       Count is {state.count}
       <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>Increment</button>
       <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>Decrement</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </div>
   )
 }
