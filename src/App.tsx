3cd37ad38e1ff2227d124eeec416a5e9370beabf
Samir Idris <samzidris11@gmail.com>
2023-05-31 00:16:48 +0100

    complete usecontext with future value

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './App.css'
-import { Box } from './components/context/Box'
-import { ThemeContextProvider } from './components/context/ThemeContext'
+import { User } from './components/context/User'
+import { UserContextProvider } from './components/context/UserContext'
 
 function App() {
   return (
     <>
-      <ThemeContextProvider>
-        <Box />
-      </ThemeContextProvider>
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
     </>
   )
 }
