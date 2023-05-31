7984a52194d3bb78c87ff1ce21815c2318834c75
Samir Idris <samzidris11@gmail.com>
2023-05-31 17:10:34 +0100

    complete restricting props

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
 import React from 'react'
 import './App.css'
-import { List } from './components/generics/List'
+import { RandomNumber } from './components/restriction/RandomNumber'
 
 function App() {
   return (
     <>
-      {/* <List
-        items={['Batman', 'Superman', 'Wonder Woman']}
-        onClick={(item) => console.log(item)}
-      />
-      <List
-        items={[1, 2, 3]}
-        onClick={(item) => console.log(item)}
-      /> */}
-      <List
-        items={[
-          {
-            id: 1,
-            first: 'Bruce',
-            last: 'Wayne'
-          },
-          {
-            id: 2,
-            first: 'Clark',
-            last: 'Kent'
-          },
-          {
-            id: 3,
-            first: 'Princess',
-            last: 'Diana'
-          }
-        ]}
-        onClick={(item) => console.log(item)}
-      />
+      <RandomNumber value={10} isPositive />
     </>
   )
 }
