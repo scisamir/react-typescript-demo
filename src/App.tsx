65c82753830f8e098262dfd90915f99c7fa245c6
Samir Idris <samzidris11@gmail.com>
2023-05-31 18:34:30 +0100

    use typescript to create a polymorphic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './App.css'
+import { Text } from './components/polymorphic/Text'
 
 function App() {
   return (
     <>
+      <Text as='h1' size='lg'>
+        Heading
+      </Text>
+      <Text as='p' size='md'>
+        Paragraph
+      </Text>
+      <Text as='label' htmlFor='someId' size='sm' color='secondary'>
+        Label
+      </Text>
     </>
   )
 }
