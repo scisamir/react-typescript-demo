ba921b2deb65d325c0c75b736359c060fd0dcb43
Samir Idris <samzidris11@gmail.com>
2023-05-31 17:59:09 +0100

    extract a component's props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import './App.css'
-import { CustomButton } from './components/html/Button'
 
 function App() {
   return (
     <>
-      <CustomButton
-        variant='primary'
-        onClick={() => console.log('Clicked')}
-      >
-        Primary Button
-      </CustomButton>
     </>
   )
 }
