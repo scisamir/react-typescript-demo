dcef79a1af106d2261823283a74004f6ec55fce4
Samir Idris <samzidris11@gmail.com>
2023-05-31 16:48:30 +0100

    use generics in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import './App.css'
-import { Private } from './components/auth/Private'
-import { Profile } from './components/auth/Profile'
+import { List } from './components/generics/List'
 
 function App() {
   return (
     <>
-      <Private isLoggedIn component={Profile} />
+      {/* <List
+        items={['Batman', 'Superman', 'Wonder Woman']}
+        onClick={(item) => console.log(item)}
+      />
+      <List
+        items={[1, 2, 3]}
+        onClick={(item) => console.log(item)}
+      /> */}
+      <List
+        items={[
+          {
+            id: 1,
+            first: 'Bruce',
+            last: 'Wayne'
+          },
+          {
+            id: 2,
+            first: 'Clark',
+            last: 'Kent'
+          },
+          {
+            id: 3,
+            first: 'Princess',
+            last: 'Diana'
+          }
+        ]}
+        onClick={(item) => console.log(item)}
+      />
     </>
   )
 }
