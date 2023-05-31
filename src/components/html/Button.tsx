f7b4ae03f417eb249677ba73e3ca01a19a62055f
Samir Idris <samzidris11@gmail.com>
2023-05-31 17:49:09 +0100

    use typescript omit keyword

--- src/components/html/Button.tsx
+++ src/components/html/Button.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 type ButtonProps = {
   variant: 'primary' | 'secondary'
-} & React.ComponentProps<'button'>
+  children: string
+} & Omit<React.ComponentProps<'button'>, 'children'>
 
 export const CustomButton = ({ variant, children, ...rest }: ButtonProps) => {
   return (
